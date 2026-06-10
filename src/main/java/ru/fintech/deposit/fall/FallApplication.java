05f6645403ded1801b0931f91d55130c36e49d93
Aleksei <morgachev_aleksey@mail.ru>
2026-06-10 21:02:19 +0300

    v.0 Создан проект

---
+++ src/main/java/ru/fintech/deposit/fall/FallApplication.java
@@ -0,0 +1,13 @@
+package ru.fintech.deposit.fall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fintech/deposit/fall/FallApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fintech.deposit.fall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
