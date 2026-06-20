489eedd33f18589191200094b050c7eb1eef1934
Aleksei <morgachev_aleksey@mail.ru>
2026-06-20 21:03:48 +0300

    v.1.6.2 Дописана реализация глобального обработчика ошибок

--- src/main/java/ru/fintech/deposit/fall/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/fintech/deposit/fall/exception/GlobalExceptionHandler.java
@@ -14,13 +14,25 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
     //Важно чтобы метод с обработкой конкретной ошибки шел перед родителем(Exception) - обработчики ищутся сверху-вниз, от конкретного к общему
     @ExceptionHandler(MethodArgumentNotValidException.class) //Если из любого контроллера(1 на приложение) вылетает MethodArgumentNotValidException(аргумент не прошел валидацию) - вызывается метод под аннотацией
-    public ResponseEntity<ErrorResponse> handleValidation (MethodArgumentNotValidException ex){ //ResponseEntity - класс с тремя компонентами HTTP-статус, Тело(здесь будет ErrorResponse), Заголовки
-        String message = String.valueOf(ex.getBindingResult().getFieldError()); //FIXME безопасность через неявность
+    public ResponseEntity<ErrorResponse> handleValidation (MethodArgumentNotValidException ex){ //Метод обработки ошибок валидации полей пользовательского ввода;ResponseEntity - класс с тремя компонентами HTTP-статус, Тело(здесь будет ErrorResponse), Заголовки
+        String message = ex
+                .getBindingResult() //Результат валидации, содержит ВСЕ ошибки
+                .getFieldErrors() //Список (List) ошибок по полям
+                .stream() //Поток, идеоматичный выбор, в отличие от цикла
+                .map(error -> error.getField() + ":" + error.getDefaultMessage()) //Лямбда-выржаение для каждой ошибки (элемента списка)
+                .findFirst() //Взять из потока первую ошибку, обернуть в Optional и вернуть
+                .orElse("VALIDATION FAILED"); //Нужен если контейнер ошибок пуст (это невозможно если уж поймана ошибка валидации), но требуется для findFrist(возвращает Optional) для обработки null
 
         ErrorResponse error = new ErrorResponse("VALIDATION_ERROR",message, LocalDateTime.now());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
-    //TODO @ExceptionHandler(Exception.class)
+    //Общий обработчик
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric (Exception ex){
+        ErrorResponse error = new ErrorResponse("ITERNAL_ERROR", "An unexpected error occurred", LocalDateTime.now()); //Безопасность через неинформативность
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
 }
